e5fee9d9fee49340aa891d812833976c27c6c5e2
Maciej Fijalkowski <fijall@gmail.com>
2016-12-14 17:31:35 +0200

    implement subscriptions

--- authsys_common/queries.py
+++ authsys_common/queries.py
@@ -2,16 +2,43 @@
 """ some common queries that can be done on the data
 """
 import time
-from sqlalchemy import select, desc, outerjoin, and_
+import datetime
+import calendar
+
+from sqlalchemy import select, desc, outerjoin, and_, func, delete
 
 from .model import members, entries, tokens, subscriptions
+
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = int(sourcedate.year + month / 12 )
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+    return datetime.datetime(year,month,day,23,00)
 
 def get_member_list(con):
     """ List all the members with whether they paid or not
     """
     s = select([members, tokens]).where(
         and_(members.c.id == tokens.c.member_id, tokens.c.valid))
-    return [(x[1],) for x in con.execute(s)]
+    return [(x[0], x[1]) for x in con.execute(s)]
+
+def get_member_data(con, no):
+    """ Get the subscription data for a single member
+    """
+    max_timestamp = list(con.execute(
+        select([func.max(subscriptions.c.end_timestamp)]).where(
+        subscriptions.c.member_id == no)))[0][0]
+    if max_timestamp is None:
+        m_id, name, tstamp = list(con.execute(select(
+            [members.c.id, members.c.name, members.c.timestamp]).where(
+            members.c.id == no)))[0]
+        return (m_id, name, tstamp, None, None)
+    return [(x[0], x[1], x[2], x[3], x[4]) for x in con.execute(
+        select([members.c.id, members.c.name, members.c.timestamp, subscriptions.c.type,
+        subscriptions.c.end_timestamp]).where(
+        and_(and_(members.c.id == no, subscriptions.c.member_id == no),
+            subscriptions.c.end_timestamp == max_timestamp)))][0]
 
 def list_indemnity_forms(con):
     """ List all the indemnity forms that have no assigned tokens
@@ -38,6 +65,30 @@ def unrecognized_entries_after(con, timestamp):
         desc(entries.c.timestamp))
     return [x[0] for x in con.execute(s)]
 
+def add_one_month_subscription(con, no, type='regular'):
+    t0 = list(con.execute(
+        select([func.max(subscriptions.c.end_timestamp)]).where(
+        subscriptions.c.member_id == no)))[0][0]
+    if t0 is None:
+        t0 = time.time()
+    end_t = time.mktime(add_months(datetime.datetime.fromtimestamp(t0), 1).timetuple())
+    con.execute(subscriptions.insert().values({
+        'member_id': no,
+        'type': type,
+        'start_timestamp': time.time(),
+        'end_timestamp': end_t,
+        }))
+
+def remove_subscription(con, no):
+    t0 = list(con.execute(
+        select([func.max(subscriptions.c.end_timestamp)]).where(
+        subscriptions.c.member_id == no)))[0][0]
+    if t0 is None or t0 < time.time():
+        return
+    id = list(con.execute(select([subscriptions.c.id]).where(
+        subscriptions.c.end_timestamp == t0)))[0][0]
+    con.execute(delete(subscriptions, subscriptions.c.id == id))
+
 def entries_after(con, timestamp):
     """ List all the entries after 'timestamp' with extra information
     about the subscription and validity
@@ -46,9 +97,9 @@ def entries_after(con, timestamp):
         entries.c.token_id == tokens.c.id, tokens.c.valid)),
         members, members.c.id == tokens.c.member_id),
         subscriptions, and_(subscriptions.c.member_id == members.c.id,
-            subscriptions.c.end_timestamp >= entries.c.timestamp))
-#        members.c.id == subscriptions.c.member_id
-    return [(a, b, c, d) for a, b, c, d in
+            and_(subscriptions.c.end_timestamp >= entries.c.timestamp,
+                subscriptions.c.start_timestamp <= entries.c.timestamp)))
+    return [(a, b, c, d, e) for a, b, c, d, e in
         con.execute(select([entries.c.token_id, members.c.name,
-        entries.c.timestamp, subscriptions.c.end_timestamp]).select_from(oj).where(
+        entries.c.timestamp, subscriptions.c.end_timestamp, subscriptions.c.type]).select_from(oj).where(
         entries.c.timestamp >= timestamp).order_by(desc(entries.c.timestamp)))]
