9e634509e23dda0ba46caecad14fdc1a4f602e2f
Maciej Fijalkowski <fijall@gmail.com>
2016-12-14 10:05:09 +0200

    some assorted work

--- authsys_common/queries.py
+++ authsys_common/queries.py
@@ -4,32 +4,51 @@
 import time
 from sqlalchemy import select, desc, outerjoin, and_
 
-from .model import members, entries, tokens
+from .model import members, entries, tokens, subscriptions
 
 def get_member_list(con):
     """ List all the members with whether they paid or not
     """
-    oj = outerjoin(members, tokens, members.c.id == tokens.c.member_id)
-    s = select([members.c.name]).select_from(oj).where(tokens.c.id != None)
-    return [x[0] for x in con.execute(s)]
+    s = select([members, tokens]).where(
+        and_(members.c.id == tokens.c.member_id, tokens.c.valid))
+    return [(x[1],) for x in con.execute(s)]
 
 def list_indemnity_forms(con):
     """ List all the indemnity forms that have no assigned tokens
     """
     oj = outerjoin(members, tokens, members.c.id == tokens.c.member_id)
-    return list(con.execute(select(
+    return [(a, b, c, d) for a, b, c, d in con.execute(select(
         [members.c.id, members.c.name, members.c.id_number,
         members.c.timestamp]).select_from(oj).where(
-        tokens.c.id == None).order_by(desc("timestamp"))))
+        tokens.c.id == None).order_by(desc("timestamp")))]
 
 def get_form(con, no):
+    """ Get indemnity form for a member 'no'
+    """
     return list(list(con.execute(select([members.c.id,
         members.c.name,
         members.c.id_number]).where(members.c.id == no)))[0])
 
 def unrecognized_entries_after(con, timestamp):
+    """ List all unrecognized entries after 'timestamp'
+    """
     oj = outerjoin(entries, tokens, entries.c.token_id == tokens.c.id)
     s = select([entries.c.token_id]).select_from(oj).where(
         and_(entries.c.timestamp >= timestamp, tokens.c.id == None)).order_by(
         desc(entries.c.timestamp))
     return [x[0] for x in con.execute(s)]
+
+def entries_after(con, timestamp):
+    """ List all the entries after 'timestamp' with extra information
+    about the subscription and validity
+    """
+    oj = outerjoin(outerjoin(outerjoin(entries, tokens, and_(
+        entries.c.token_id == tokens.c.id, tokens.c.valid)),
+        members, members.c.id == tokens.c.member_id),
+        subscriptions, and_(subscriptions.c.member_id == members.c.id,
+            subscriptions.c.end_timestamp >= entries.c.timestamp))
+#        members.c.id == subscriptions.c.member_id
+    return [(a, b, c, d) for a, b, c, d in
+        con.execute(select([entries.c.token_id, members.c.name,
+        entries.c.timestamp, subscriptions.c.end_timestamp]).select_from(oj).where(
+        entries.c.timestamp >= timestamp).order_by(desc(entries.c.timestamp)))]
--- tests/test_queries.py
+++ tests/test_queries.py
@@ -13,7 +13,7 @@ class TestQueries(object):
         populate_with_test_data(cls.c)
 
     def test_list_members(self):
-        assert q.get_member_list(self.c) == ["John One"]
+        assert q.get_member_list(self.c) == [("John One",)]
 
     def test_list_forms(self):
         assert q.list_indemnity_forms(self.c) == [
@@ -21,12 +21,29 @@ class TestQueries(object):
             (3, u'Brad Two', u'11111', 1236),
             (4, u'Jim Three', u'xyz', 1237)]
 
+    def test_unrecognized_entries_after(self):
+        t0 = 10000
+        tok1 = "A" * 6 + "08"
+        tok4 = "D" * 6 + "08"
+        assert q.unrecognized_entries_after(self.c, t0) == [
+            tok4, tok1, tok1]
+        assert q.unrecognized_entries_after(self.c, t0 + 20) == [
+            tok4]
+
     def test_entries_after(self):
         t0 = 10000
         tok1 = "A" * 6 + "08"
+        tok2 = "B" * 6 + "08"
         tok3 = "C" * 6 + "08"
         tok4 = "D" * 6 + "08"
-        assert q.unrecognized_entries_after(self.c, t0) == [
-            tok4, tok3, tok1, tok1]
-        assert q.unrecognized_entries_after(self.c, t0 + 20) == [
-            tok4, tok3]
+        all_entries = [(tok1, None, t0, None),
+            (tok1, None, t0 + 5, None),
+            (tok2, "John One", t0 + 10, 10020),
+            (tok3, None, t0 + 20, None),
+            (tok4, None, t0 + 30, None),
+            (tok2, "John One", t0 + 40, None)
+        ]
+        all_entries.reverse()
+
+        assert q.entries_after(self.c, t0) == all_entries
+        assert q.entries_after(self.c, t0 + 10) == all_entries[:-2]
