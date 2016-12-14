66761d130a55fb39e4e785d0f83b96699267fd8c
Maciej Fijalkowski <fijall@gmail.com>
2016-12-14 17:31:35 +0200

    implement subscriptions

--- tests/test_queries.py
+++ tests/test_queries.py
@@ -13,7 +13,7 @@ class TestQueries(object):
         populate_with_test_data(cls.c)
 
     def test_list_members(self):
-        assert q.get_member_list(self.c) == [("John One",)]
+        assert q.get_member_list(self.c) == [(2, "John One",)]
 
     def test_list_forms(self):
         assert q.list_indemnity_forms(self.c) == [
@@ -36,14 +36,18 @@ class TestQueries(object):
         tok2 = "B" * 6 + "08"
         tok3 = "C" * 6 + "08"
         tok4 = "D" * 6 + "08"
-        all_entries = [(tok1, None, t0, None),
-            (tok1, None, t0 + 5, None),
-            (tok2, "John One", t0 + 10, 10020),
-            (tok3, None, t0 + 20, None),
-            (tok4, None, t0 + 30, None),
-            (tok2, "John One", t0 + 40, None)
+        all_entries = [(tok1, None, t0, None, None),
+            (tok1, None, t0 + 5, None, None),
+            (tok2, "John One", t0 + 10, 10020, "regular"),
+            (tok3, None, t0 + 20, None, None),
+            (tok4, None, t0 + 30, None, None),
+            (tok2, "John One", t0 + 40, None, None)
         ]
         all_entries.reverse()
 
         assert q.entries_after(self.c, t0) == all_entries
         assert q.entries_after(self.c, t0 + 10) == all_entries[:-2]
+
+    def test_get_member_data(self):
+        assert q.get_member_data(self.c, 2) == (2, "John One", 1235, "regular", 10020)
+        assert q.get_member_data(self.c, 1) == (1, "One Two", 1234, None, None)
